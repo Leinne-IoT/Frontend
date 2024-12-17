f02d6c3311f8e5a98349a59d7d18aa5bd5e7427e
LeinneSW <nno88551@naver.com>
2024-12-17 16:47:35 +0900

    localStorage 에 json을 읽고 쓰는 기능 추가

--- src/ui/utils/LocalStorage.tsx
+++ src/ui/utils/LocalStorage.tsx
@@ -4,7 +4,7 @@ export const useStorageState = <T,>(key: string, defaultValue: T): [T, (value: T
     const isNumber = typeof defaultValue === "number";
     const [storageValue, setStorageValue] = useState<T>(() => {
         let data: any = window.localStorage.getItem(key);
-        if(data !== null){
+        if(data != null){
             data = isNumber ? +data : data;
             return data as T;
         }
@@ -15,6 +15,26 @@ export const useStorageState = <T,>(key: string, defaultValue: T): [T, (value: T
         const newValue: any = isNumber ? +value : value;
         setStorageValue(newValue);
         window.localStorage.setItem(key, String(newValue));
+    };
+    return [storageValue, setValue];
+};
+
+export const useStorageJsonState = <T,>(key: string, defaultValue: T): [T, (value: T) => void] => {
+    const [storageValue, setStorageValue] = useState<T>(() => {
+        try{
+            const data: any = window.localStorage.getItem(key);
+            if(data){
+                return JSON.parse(data)
+            }
+        }catch{
+            window.localStorage.setItem(key, JSON.stringify(defaultValue));
+        }
+        return defaultValue;
+    });
+
+    const setValue = (value: T) => {
+        setStorageValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
     };
     return [storageValue, setValue];
 };
