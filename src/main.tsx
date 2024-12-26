4dc1b6a46043412b6177ce2b6b84593c4efc5271
LeinneSW <nno88551@naver.com>
2024-12-26 10:40:04 +0900

    차트 누락내용 커밋

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,39 @@
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import {
+    Chart,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from "chart.js";
+import {CandlestickController} from "chartjs-chart-financial";
+
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {ToastContainer} from "react-toastify";
-import {CandlestickController} from "chartjs-chart-financial";
 
 import App from './ui/layout/app/App.tsx'
 import {DataProvider} from "./feature/provider/DataProvider.tsx";
 import {AuthProvider} from "./feature/provider/AuthProvider.tsx";
+
+// Chart.js 플러그인 등록
+Chart.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    CandlestickController,
+);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
